2b8547e534fa9c2ae844c6d2115c2b2147880075
Henrik Drefs <henrik.drefs@outlook.de>
2019-08-05 20:54:20 +0200

    resolved #41

--- src/extractor/amount.ts
+++ src/extractor/amount.ts
@@ -68,7 +68,7 @@ export class AmountExtractor extends Extractor<Amount> {
  with no leading zero where S also matches as 5
  3. line end, space or dash
  */
-const amountValuePattern = /(?:^|\s|\*)-?((?:[1-9]\d+|\d)\s?[,.]\s?[\dS]{2})(?:[\-\s]|$)/gim;
+const amountValuePattern = /(?:^|\s|\*)-?((?:[1-9]\d+|\d)\s?[,.]\s?[\dS]{2})(?:[\-\s€]|$)/gim;
 
 const illegalPreviousLinePatterns = [/MwSt$/i];
 
