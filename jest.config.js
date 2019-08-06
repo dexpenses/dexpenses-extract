95c1d0d73d9fe3c90029f04703766a0855222cba
Henrik Drefs <henrik.drefs@outlook.de>
2019-08-06 16:57:44 +0200

    jest watches test data

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,14 @@ module.exports = {
     '^.+\\.tsx?$': 'ts-jest',
   },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleFileExtensions: [
+    'ts',
+    'tsx',
+    'js',
+    'jsx',
+    'json',
+    'node',
+    'txt',
+    'inactive',
+  ],
 };
